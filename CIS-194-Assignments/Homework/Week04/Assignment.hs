4287143ec6443c66ee01f319c84da18a99a5da25
SamFieldman1997 <sam.a.fieldman@gmail.com>
2020-09-08 14:43:17 -0400

    Assignment 4 finished. Also added simple implimentation of Max subset sum

--- CIS-194-Assignments/Homework/Week04/Assignment.hs
+++ CIS-194-Assignments/Homework/Week04/Assignment.hs
@@ -1,90 +1,116 @@
 module Homework.Week04.Assignment (
-  ex1,
-  ex2,
-  ex3,
-  ex4,
-  ex5,
-  ex6,
-  ex7,
-  ex8,
-  ex9,
-  ex10,
-  ex11,
-  ex12,
-  insertBST,
-  allCaps,
-  dropTrailingWhitespace,
-  firstLetters,
-  asList,
-  BST(..)
+    fun1,
+    fun2,
+    testFun1,
+    testFun2,
+    foldTree,
+    xor,
+    map',
+    sieveSundaram
 ) where
 
-import Homework.Week04.BST
+import Data.List
 
--- #1
-ex1 :: a -> b -> b
-ex1 = undefined
 
--- #2
-ex2 :: a -> a -> a
-ex2 = undefined
+-- Exercise 1: Wholemeal Programming
+-- reimpliment recursive functions using wholemeal programming style -> utilize the core library
+fun1 :: [Integer] -> Integer
+fun1 = foldl' (*) 1 . map (subtract 2) . filter even 
 
--- #3
-ex3 :: Int -> a -> a
-ex3 = undefined
+fun2 :: Integer -> Integer
+fun2 = foldl' (+) 0 . filter even . takeWhile (/=0) . iterate step
 
--- #4
-ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+step :: Integer -> Integer
+step x | x == 1    = 0
+       | even x    = div x 2
+       | otherwise = 3*x + 1
 
--- #5
-ex5 :: Bool -> Bool
-ex5 = undefined
+-- Original implimentation in the assignment. used to test the above.
+testFun1 :: [Integer] -> Integer
+testFun1 [] = 1
+testFun1 (x:xs)
+    | even x    = (x-2) * testFun1 xs
+    | otherwise = testFun1 xs
 
--- #6
-ex6 :: (a -> a) -> a
-ex6 = undefined
+testFun2 :: Integer -> Integer
+testFun2 1 = 0
+testFun2 x
+    | even x    = x + testFun2 (div x 2) 
+    | otherwise = testFun2 (3*x + 1)
 
--- #7
-ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+-- Exercise 2: Tree folding Fun!
 
--- #8
-ex8 :: [a] -> [a]
-ex8 = undefined
+data Tree a  = Leaf
+             | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
 
--- #9
-ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+foldTree :: [a] -> Tree a
+foldTree = foldr insertInTree Leaf
 
--- #10
-ex10 :: Maybe a -> a
-ex10 = undefined
+insertInTree :: a -> Tree a -> Tree a
+insertInTree x Leaf = Node 0 Leaf x Leaf
+insertInTree x (Node h left y right)
+    | hLeft < hRight   = Node h           nLeft y right  -- left subtree is smaller than right -> add to left 
+    | hLeft > hRight   = Node h           left  y nRight -- right subtree is smaller than left -> add to right 
+    | nhLeft < nhRight = Node h           nLeft y right  -- left and right subtrees are equal height. Add to tree with fewer nodes
+    | otherwise        = Node (nhRight+1) left  y nRight -- the tree is full -> add another level at the leaves
+  where hLeft   = heightTree left
+        hRight  = heightTree right
+        nLeft   = insertInTree x left
+        nRight  = insertInTree x right
+        nhLeft  = heightTree nLeft
+        nhRight = heightTree nRight
 
--- #11
-ex11 :: a -> Maybe a
-ex11 = undefined
+heightTree :: Tree a -> Integer
+heightTree Leaf = 0
+heightTree (Node height _ _ _) = height
 
--- #12
-ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+showTree :: Show a => Tree a -> String
+showTree Leaf = ""  
+showTree n@(Node i _ _ _) = go i n
+  where
+    go _ (Leaf) = "" 
+    go i (Node _ l c r) = go (i-1) l ++ 
+        replicate (4*fromIntegral i) ' ' ++ show c ++ "\n" ++ go (i-1) r
 
--- #13
-insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
 
--- #14
-allCaps :: [String] -> Bool
-allCaps = undefined
+-- Exercise 3: More Folds!
+-- Impliment the given functions utilizing folding
 
--- #15
-dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+-- filter out the odd values, use foldl to get list length, check parity.
+xor :: [Bool] -> Bool
+xor = odd . foldl' (\acc _ -> acc + 1) 0 . filter id
 
--- #16
-firstLetters :: [String] -> [Char]
-firstLetters = undefined
+-- from right to left, add the mapped value to the front of the accumulator list.
+-- Note, no seperate case is needed for the case a null list is passed, since foldr handles this properly.
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\y ys -> (f y):ys) [] 
 
--- #17
-asList :: [String] -> String
-asList = undefined
+-- reimpliment filter for fun d: same idea as above, but do not append if function evaluates to false
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f = foldr (\x xs -> if f x then x:xs else xs) []
+
+
+-- Exercise 4: Finding Primes
+-- Impliment the sieve of Sundaram
+
+-- filter numbers with list comprehension. map the resulting list of numbers to generate the resulting primes.
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map (\i -> 2*i+1) $ [j | j <- [1..n], not $ elem j numbers] 
+  where numbers = numbersToRemove n
+
+numbersToRemove :: Integer -> [Integer]
+numbersToRemove = flip takeWhile (map (\(a,b) -> a+b+2*a*b) [(i,j) | i<-[1..], j<-[1..]]) . belowN
+
+-- enable pointfree style by defining a boolean function with the integer passed into the sieve.
+belowN :: Integer -> (Integer -> Bool)
+belowN n = (<n)
+
+
+-- Impliment Max Subarray Sum 
+maxSubArray :: [Integer] -> Integer
+maxSubArray xs = snd $ foldl' next_pair (0, 0) xs
+
+next_pair :: (Integer, Integer) -> Integer -> (Integer, Integer)
+next_pair (c_sum, b_sum) x = (next_sum, max b_sum next_sum)
+  where next_sum = max 0 (c_sum + x)
